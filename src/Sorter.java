d0940dac6f46f02b1b2ad287eb547a5ff32536cc
SaudMaashi <saudm45q@gmail.com>
2024-02-09 20:32:45 +0300

    Adding Selection Sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,26 @@ public class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public class SelectionSort {
+        public static void selectionSort(int[] arr) {
+            int n = arr.length;
+
+            // Traverse through all array elements
+            for (int i = 0; i < n - 1; i++) {
+                // Find the minimum element in the unsorted part of the array
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the found minimum element with the first element
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
 }
